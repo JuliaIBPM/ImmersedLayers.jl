8bf6a336883da5435e50c4070de77c96325f07e6
Jeff Eldredge <jdeldre@g.ucla.edu>
2021-09-07 14:30:41 -0700

    Extended the plot recipe so that it accepts a layers argument to plot points

--- src/plot_recipes.jl
+++ src/plot_recipes.jl
@@ -1,14 +1,46 @@
 using RecipesBase
 using ColorTypes
-using CartesianGrids
-using RigidBodyTools
+#using CartesianGrids
+#using RigidBodyTools
 
 
-@recipe function f(w::T,cache::BasicILMCache) where {T<:GridData}
+@recipe function f(w::T,cache::BasicILMCache; layers=false) where {T<:GridData}
+
+  framestyle --> :frame
+  xlims --> (-Inf,Inf)
+  ylims --> (-Inf,Inf)
+  aspect_ratio := 1
+  legend --> :none
+  grid --> false
+
   @series begin
     trim := 2
     w, cache.g
   end
+
+  if layers
+    pts = points(cache)
+    #x = [pts.u; pts.u[1]]
+    #y = [pts.v; pts.v[1]]
+    x = pts.u
+    y = pts.v
+    @series begin
+      seriestype --> :scatter
+      markersize --> 1
+      markershape --> :circle
+      markercolor --> :black
+      #linecolor --> :black
+      #fillrange --> 0
+      #fillalpha --> 0
+      #fillcolor --> :black
+      x := x
+      y := y
+      ()
+    end
+  end
+
+
+
 end
 
 @recipe function f(w::T,sys::ILMSystem) where {T<:GridData}
@@ -41,3 +73,19 @@ end
     end
 
 end
+
+#=
+@recipe function f(b::Body)
+    x = [b.x; b.x[1]]
+    y = [b.y; b.y[1]]
+    linecolor --> mygreen
+    fillrange --> 0
+    fillcolor --> mygreen
+    aspect_ratio := 1
+    legend := :none
+    grid := false
+    x := x
+    y := y
+    ()
+end
+=#
