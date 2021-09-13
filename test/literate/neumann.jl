adc15c365bfdcc4ccf8d8543253e2044988755da
Jeff Eldredge <jdeldre@g.ucla.edu>
2021-09-13 16:08:50 -0700

    Refined the neumann doc

--- test/literate/neumann.jl
+++ test/literate/neumann.jl
@@ -26,24 +26,34 @@ derivative.
 
 Discretizing this problem by the usual techniques, we seek to solve
 
-$$\begin{bmatrix} L & D_s \\ G_s & R_n^T R_n \end{bmatrix} \begin{pmatrix} f \\ -d \end{pmatrix} = \begin{pmatrix} R s \\ \overline{v}_n \end{pmatrix}$$
+$$\begin{bmatrix} L & D_s \\ G_s & R_n^T R_n \end{bmatrix} \begin{pmatrix} f \\ -[\phi] \end{pmatrix} = \begin{pmatrix} R [v_n] \\ \overline{v}_n \end{pmatrix}$$
 
-where $\overline{v}_n = (v^+_n + v^-_n)/2$ and $s = v^+_n - v^-_n$. The resulting
-$d$ is $f^+-f^-$.
+where $\overline{v}_n = (v^+_n + v^-_n)/2$ and $[v_n] = v^+_n - v^-_n$. The resulting
+$[\phi]$ is $f^+-f^-$.
 
 As with the Dirichlet problem, this saddle-point problem can be solved by block-LU decomposition. First solve
 
-$$L f^{*} = R s$$
+$$L f^{*} = R [v_n]$$
 
 for $f^*$. Then solve
 
-$$-S d = \overline{v}_n - G_s f^{*}$$
+$$-S [\phi] = \overline{v}_n - G_s f^{*}$$
 
-for $d$, where $S = R_n^T R_n - G_s L^{-1} D_s = -C_s L^{-1}C_s^T$, and finally, compute
+for $[\phi]$, where $S = R_n^T R_n - G_s L^{-1} D_s = -C_s L^{-1}C_s^T$, and finally, compute
 
-$$f = f^{*} + L^{-1}D_s d$$
+$$f = f^{*} + L^{-1}D_s [\phi]$$
 
-We will demonstrate these steps here.
+It should be remembered that, for any scalar potential field, there is
+a corresponding streamfunction $\psi$ that generates the same flow. We
+can get that field, as well, with only a little bit more effort:
+
+$$S [\psi] = C_s f^{*}$$
+
+and then solve
+
+$$L s = C_s^T [\phi] - \hat{C}_s^T [\psi]$$.
+
+for the streamfunction $s$.
 =#
 
 using ImmersedLayers
@@ -68,10 +78,12 @@ end
 The extra cache holds additional intermediate data, as well as
 the Schur complement. We don't bother creating a filtering matrix here.
 =#
-struct NeumannPoissonCache{SMT,ST,FT} <: AbstractExtraILMCache
+struct NeumannPoissonCache{SMT,DVT,VNT,FT,ST} <: AbstractExtraILMCache
    S :: SMT
-   vn :: ST
+   dvn :: DVT
+   vn :: VNT
    fstar :: FT
+   sstar :: ST
 end
 #=
 The function `prob_cache`, as before, constructs the operators and extra
@@ -79,36 +91,55 @@ cache data structures
 =#
 function ImmersedLayers.prob_cache(prob::NeumannPoissonProblem,base_cache::BasicILMCache)
     S = create_CLinvCT(base_cache)
+    dvn = zeros_surface(base_cache)
     vn = zeros_surface(base_cache)
     fstar = zeros_grid(base_cache)
-    NeumannPoissonCache(S,vn,fstar)
+    sstar = zeros_gridcurl(base_cache)
+    NeumannPoissonCache(S,dvn,vn,fstar,sstar)
 end
-
+nothing #hide
 #=
 And finally, here's the steps we outlined above, used to
 extend the `solve` function
 =#
 function ImmersedLayers.solve(vnplus,vnminus,prob::NeumannPoissonProblem,sys::ILMSystem)
     @unpack extra_cache, base_cache = sys
-    @unpack S, vn, fstar = extra_cache
+    @unpack S, dvn, vn, fstar, sstar = extra_cache
+
+    fill!(fstar,0.0)
+    fill!(sstar,0.0)
 
     f = zeros_grid(base_cache)
-    d = zeros_surface(base_cache)
+    s = zeros_gridcurl(base_cache)
+    df = zeros_surface(base_cache)
+    ds = zeros_surface(base_cache)
 
-    regularize!(fstar,vnplus-vnminus,base_cache)
     vn .= 0.5*(vnplus+vnminus)
+    dvn .= vnplus - vnminus
 
+    regularize!(fstar,dvn,base_cache)
     inverse_laplacian!(fstar,base_cache)
 
-    surface_grad!(d,fstar,base_cache)
-    d .= vn - d
-    d .= -(S\d);
+    surface_grad!(df,fstar,base_cache)
+    df .= vn - df
+    df .= -(S\df);
 
-    surface_divergence!(f,d,base_cache)
+    surface_divergence!(f,df,base_cache)
     inverse_laplacian!(f,base_cache)
-    f .+= fstar;
+    f .+= fstar
 
-    return f, d
+    surface_curl!(sstar,df,base_cache)
+
+    surface_grad_cross!(ds,fstar,base_cache)
+    ds .= S\ds
+
+    surface_curl_cross!(s,ds,base_cache)
+    s .-= sstar
+    s .*= -1.0
+
+    inverse_laplacian!(s,base_cache)
+
+    return f, df, s, ds
 end
 nothing #hide
 
@@ -147,17 +178,18 @@ nothing #hide
 Solve it
 =#
 solve(vnplus,vnminus,prob,sys) #hide
-@time f, d = solve(vnplus,vnminus,prob,sys);
+@time f, df, s, ds = solve(vnplus,vnminus,prob,sys);
 
 #=
 and plot the field
 =#
-plot(f,sys,levels=30)
+plot(plot(f,sys,layers=true,levels=30,title="ϕ"),
+plot(s,sys,layers=true,levels=30,title="ψ"))
 
 #=
-and the Lagrange multiplier field, $d$, on the surface
+and the Lagrange multiplier field, $[\phi]$, on the surface
 =#
-plot(d)
+plot(ds)
 
 #=
 ## Multiple bodies
@@ -202,15 +234,21 @@ copyto!(vnplus,nrm.u,sys,2)
 #=
 Solve it and plot
 =#
-f, d  = solve(vnplus,vnminus,prob,sys)
-plot(f,sys)
+f, df, s, ds  = solve(vnplus,vnminus,prob,sys)
+plot(plot(f,sys,layers=true,levels=30,title="ϕ"),
+plot(s,sys,layers=true,levels=30,title="ψ"))
 
 #=
 Now, let's compute the added mass components of the circle associated
 with this motion. We are approximating
 
-$$M = \int_{C_2} f^+ \mathbf{n}\mathrm{d}s$$
+$$M = -\int_{C_2} f^+ \mathbf{n}\mathrm{d}s$$
 
-where $C_2$ is shape 2 (the circle), and $f^+$ is simply $d$ on body 2.
+where $C_2$ is shape 2 (the circle), and $f^+$ is simply $[\phi]$ on body 2.
 =#
-M = integrate(d∘nrm,sys,2)
+M = -integrate(df∘nrm,sys,2)
+
+#=
+As one would expect, the circle has added mass in the $x$ direction
+associated with moving in that direction.
+=#
